b1d1168fcbb9db677e2fd8251991c9dace87cf0d
borisdering <dbrsdbn@googlemail.com>
2018-01-30 16:41:50 +0100

    Made tick() accessible to override.

--- APNGKit/APNGImageView.swift
+++ APNGKit/APNGImageView.swift
@@ -237,7 +237,7 @@ open class APNGImageView: APNGView {
         timer = nil
     }
     
-    func tick() {
+    open func tick() {
         guard let image = image else {
             return
         }
